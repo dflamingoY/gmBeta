d1def1521f2544c1657d4617d40b5c640f8f9d95
mingo <ym199210@163.com>
2020-08-24 12:50:25 +0800

    ..

--- app/src/main/java/org/xiaoxingqi/gmdoc/core/http/HttpServer.java
+++ app/src/main/java/org/xiaoxingqi/gmdoc/core/http/HttpServer.java
@@ -86,8 +86,8 @@ public class HttpServer {
                     .getSocketFactory();
             OkHttpClient okHttpClient = new OkHttpClient();
             okHttpClient = okHttpClient.newBuilder()
-//                    .addInterceptor(new ReceiveIntercept(mContext))
-//                    .addInterceptor(new RequestHeadInterceptot(mContext))
+                    .addInterceptor(new ReceiveIntercept(mContext))
+                    .addInterceptor(new RequestHeadInterceptot(mContext))
                     .connectTimeout(20, TimeUnit.SECONDS)
                     .readTimeout(20, TimeUnit.SECONDS)
                     .cookieJar(new LocalCook())
